c46c4b4e95236e4313f04eeefb1ab714c86a0cd5
barbass <barbass1025@gmail.com>
2014-05-13 23:44:08 +0600

    Возможность указывать главы

--- application/views/footer.php
+++ application/views/footer.php
@@ -4,6 +4,7 @@
 	
 	<footer>
 		<div class='container'>
+			<p class='text-muted'>&nbsp;&nbsp; <a href="mailto:barbass1025@gmail.com">barbass1025@gmail.com</a></p>
 		</div>
 	</footer>
 
--- application/views/main.php
+++ application/views/main.php
@@ -1,6 +1,6 @@
 <div class="col-sm-9 col-md-10 col-lg-12 main">
 		
-	<h2 class="form-signin-heading heading">Скачивание</h2>
+	<h2 class="form-signin-heading heading">Скачивание манги</h2>
 		
 	<?php if (!empty($error)) { ?>
 		<div class="alert alert-dismissable alert-danger">
@@ -15,38 +15,49 @@
 			<?php echo $success;?>
 		</div>
 	<?php } ?>
-		
-	<br>
 	
-	<div>
+	<div class='block'>
 		<div>
 			Сайт: <input id="site" type='url' class="form-control" value='' autocomplete='on' maxlength='150' required placeholder="http://site.com/">
 			<br>
-			<button id="get_manga_info" class='btn btn-primary'>Инфо</button>
+			<button id="get_manga_info" class='btn btn-primary'>
+				<span class="glyphicon glyphicon-eye-open"></span>
+				Инфо
+			</button>
 		</div>
 		
-		<br><br>
+		<br>
 		
 		<div>
 			Папка: <input id="folder" type='url' class="form-control" value='' autocomplete='on' maxlength='150' placeholder="D:\manga">
 			<br>
-			<button id="get_manga_folder" class='btn btn-primary'>Найти</button>
+			<button id="get_manga_folder" class='btn btn-primary'>
+				<span class="glyphicon glyphicon-search"></span>
+				Найти
+			</button>
 		</div>
 	</div>
 
-	<br><br><br>
-	
-	<div>
-		<button id="download_manga_chapter_image_list" class='btn btn-primary'>Получить списки изображений</button>
+	<div class='block'>
+		<button id="download_manga_chapter_image_list" class='btn btn-primary'>
+			<span class="glyphicon glyphicon-list-alt"></span>
+			Получить списки изображений
+		</button>
+		&nbsp;&nbsp;&nbsp;
+		<button id="download_manga_chapter_list" class='btn btn-primary'>
+			<span class="glyphicon glyphicon-download-alt"></span>
+			Сохранить главы (изображения)
+		</button>
 	</div>
-	
-	<br><br>
-	
-	<div>
-		<button id="download_manga_chapter_list" class='btn btn-primary'>Сохранить главы (изображения)</button>
+
+	<div class='block'>
+		<textarea id='select_chapter' class='form-control' placeholder='Укажите главы через , или -. Пример: 1/1,1/2,2/3-3/10'></textarea>
+		<br>
+		<button id="button_select_chapter" class='btn btn-primary'>
+			<span class="glyphicon glyphicon-th-list"></span>
+			Выбрать главы
+		</button>
 	</div>
-	
-	<br><br>
 	
 	<table id="manga" class='table'>
 		<tr>
@@ -501,13 +512,100 @@
 			});
 		}
 		
-		// Выбор чекбокосов всех
+		// Выбор чекбоксов всех
 		$('#checked_all_checkbox_download').on('change', function() {
 			if ($(this).attr('checked') === 'checked') {
 				$('#manga input.checkbox_download').attr('checked', 'checked');
 			} else {
 				$('#manga input.checkbox_download').removeAttr('checked');
 			}
+		});
+		
+		// Выбор указанных глав
+		$('#button_select_chapter').on('click', function() {
+			// TODO: обработка глав
+			$('#manga input.checkbox_download').removeAttr('checked');
+
+			// фомат: 1/1, 1/2, 1/3-2/1
+			var text = $('#select_chapter').val();
+			var pre_chapters = text.split(',');
+			
+			var time = new Date().getMilliseconds();
+			
+			for(var key in pre_chapters) {
+				pre_chapters[key] = pre_chapters[key].trim();
+				
+				var tere = pre_chapters[key].match('-');
+				// если 1/1
+				if (tere == null) {
+					$('#manga .chapter .chapter_link[data-href $= "vol'+pre_chapters[key]+'"]').parents('tr').eq(0).find('input.checkbox_download').attr('checked', 'checked');
+				} else {
+					// парсим границы
+					var border = pre_chapters[key].split('-');
+					if (!border[0] || !border[1]) {
+						continue;
+					} else {
+						border[0] = border[0].trim();
+						var start = border[0].split('/');
+						// если указано без главы (1)
+						if (!start[1]) {
+							start[1] = -1;
+						}
+
+						border[1] = border[1].trim();
+						var end = border[1].split('/');
+						// если указано без главы (1)
+						if (!end[1]) {
+							end[1] = -1;
+						}
+						
+						var vol_start = parseInt(start[0]);
+						var chapter_start = parseInt(start[1]);
+						var vol_end = parseInt(end[0]);
+						var chapter_end = parseInt(end[1]);
+						
+						for(var i = vol_start; i <= vol_end; i++) {
+							// если не указаны главы, то ищем сразу по томам
+							if ((i != vol_start && i != vol_end) || 
+								(i == vol_end && chapter_end == -1) ||
+								(i == vol_start && chapter_start == -1)
+							) {
+								var a_chapter = $('#manga .chapter .chapter_link[data-href *= "vol'+i+'/"]');
+								
+								$(a_chapter).each(function(index, a) {
+									$(a).parents('tr').eq(0).find('input.checkbox_download').attr('checked', 'checked');
+								});
+								
+								continue;
+							}
+							
+							// если указаны главы
+							var a_chapter = $('#manga .chapter .chapter_link[data-href *= "vol'+i+'/"]');
+							//Пробегаем по ссылке тома, сравниваем номер главы с границами
+							$(a_chapter).each(function(index, a) {
+								var href = $(a).attr('data-href');
+								var href = href.split('vol'+i+'/');
+								if (!href[1]) {
+									href[1] = -1;
+								}
+								var ch = parseInt(href[1]);
+								// если глава попадает под границы, то отмечаем
+								if ((i == vol_start && ch >= chapter_start) || 
+									(i == vol_end && ch <= chapter_end)
+								) {
+									$(a).parents('tr').eq(0).find('input.checkbox_download').attr('checked', 'checked');
+								}
+								
+							});
+						}
+						
+					}
+				}
+			}
+			
+			var time_end = new Date().getMilliseconds();
+			console.log('time='+(time_end - time));
+			
 		});
 		
 	});
